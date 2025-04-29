8e67503e3f8d21bc8afe65bf358431371ba4660c
Luiz <luizlhdev@gmail.com>
2025-04-29 17:59:08 -0300

    Senha do mysql

--- src/main/java/pdv/constants/CommonConstants.java
+++ src/main/java/pdv/constants/CommonConstants.java
@@ -2,7 +2,7 @@ package pdv.constants;
 
 public class CommonConstants {
     public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/schema_db";
-    public static final String DB_USERNAME = "lhdev";
-    public static final String DB_PASSWORD = "medico1133";
+    public static final String DB_USERNAME = "";
+    public static final String DB_PASSWORD = "";
     public static final String DB_USERS_TABLE_NAME = "users";
 }
